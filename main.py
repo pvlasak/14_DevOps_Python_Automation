886acf43efc701b25d61d727adf9bbef94f4a716
pvlasak <petr.vlasakk@gmail.com>
2026-06-30 17:01:35 +0200

    vpc in eu-west-3 region created by resource definition

--- main.py
+++ main.py
@@ -1,6 +1,24 @@
 import boto3
 
-client = boto3.client('ec2')
+client = boto3.client('ec2', region_name='us-east-1')
+resource = boto3.resource('ec2', region_name='eu-west-3')
+
+vpc_paris = resource.create_vpc(
+    CidrBlock='10.0.0.0/16'
+)
+vpc_paris.create_subnet(
+    CidrBlock='10.0.10.0/24',
+)
+vpc_paris.create_subnet(
+    CidrBlock='10.0.20.0/24',
+)
+vpc_paris.create_tags(
+    Tags=[
+        {'Key': 'Name',
+        'Value': 'Paris-VPC'
+         },
+    ]
+)
 
 all_available_vpcs = client.describe_vpcs()
 all_vpcs = all_available_vpcs['Vpcs']
