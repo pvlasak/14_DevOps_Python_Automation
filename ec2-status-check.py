ef6aa5dfd540dfa14d1798b5d3770784ef928d14
pvlasak <petr.vlasakk@gmail.com>
2026-06-30 19:33:04 +0200

    instance and system statuses obtained

--- ec2-status-check.py
+++ ec2-status-check.py
@@ -9,3 +9,9 @@ print(instances_description)
 for reservation in instances_description['Reservations']:
     for instance in reservation['Instances']:
         print(f"Status of instance {instance["InstanceId"]} is {instance['State']['Name']}")
+
+statuses = ec2_client.describe_instance_status()
+for status in statuses['InstanceStatuses']:
+    instance_status = status['InstanceStatus']['Status']
+    system_status = status['SystemStatus']['Status']
+    print(f"Instance {status['InstanceId']} status is {instance_status} and system status is {system_status}")
